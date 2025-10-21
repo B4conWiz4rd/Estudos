e882ab2d7bb989bb4fc8656f0c0ec4bba958f917
B4conWiz4rd <137074816+B4conWiz4rd@users.noreply.github.com>
2025-10-20 22:06:22 -0300

    alguns updates

--- IHS/Linguagem de Programação - LP/aula12/cod05.py
+++ IHS/Linguagem de Programação - LP/aula12/cod05.py
@@ -1,6 +1,12 @@
 import pyautogui
 import time
 
+pyautogui.hotkey("win", "r")
+time.sleep(1)
+pyautogui.write("mspaint")
+time.sleep(1)
+pyautogui.hotkey("enter")
+
 time.sleep(5)
 pyautogui.moveTo(x=429, y=381)
 
@@ -9,13 +15,13 @@ pyautogui.dragRel(100,0, duration=2)
 pyautogui.dragRel(0,-100, duration=2)
 pyautogui.dragRel(-100,0, duration=2)
 
-pyautogui.moveTo(x=429, y=481)
+pyautogui.moveTo(x=629, y=381)
 pyautogui.dragRel(0,100, duration=2)
 pyautogui.dragRel(100,0, duration=2)
 pyautogui.dragRel(0,-100, duration=2)
 pyautogui.dragRel(-100,0, duration=2)
 
-pyautogui.moveTo(x=429, y=581)
+pyautogui.moveTo(x=829, y=381)
 pyautogui.dragRel(0,100, duration=2)
 pyautogui.dragRel(100,0, duration=2)
 pyautogui.dragRel(0,-100, duration=2)
