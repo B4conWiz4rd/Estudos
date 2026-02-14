efa3e6f2fa27c23f73b7e3ae0ceda3e5e7026112
Eddy <137074816+B4conWiz4rd@users.noreply.github.com>
2026-02-13 22:05:35 -0300

    13/02

--- IHS/Linguagem Orientada a Objetos - LOO/atividades/lista_exercicios/exer006.php
+++ IHS/Linguagem Orientada a Objetos - LOO/atividades/lista_exercicios/exer006.php
@@ -1,4 +1,6 @@
 <?php
     $registroPeso = $altv[1];
     $registroAltura = $altv[2];
-    
+    $valorImc = ($registroAltura*$registroAltura);
+    $valorImc = $registroPeso/$valorImc;
+    echo ("O valor do IMC é de $valorImc kg/m²");
