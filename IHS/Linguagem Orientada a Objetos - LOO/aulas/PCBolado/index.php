5e0130c199df92c796a3bfc3eddfa296f320766f
Eddy <137074816+B4conWiz4rd@users.noreply.github.com>
2026-06-27 11:28:45 -0300

    27/06

--- IHS/Linguagem Orientada a Objetos - LOO/aulas/PCBolado/index.php
+++ IHS/Linguagem Orientada a Objetos - LOO/aulas/PCBolado/index.php
@@ -1,80 +1,14 @@
 <?php
+    $db = __DIR__ . "/src/data/database.sqlite";
+    $pdo = new PDO("sqlite:$db");
+    //evita que algum erro do banco de dados, acabe com o funcionamento total do site.
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $computadoFunc = [
-        [
-            'nome' => "Daten 01",
-            'descricao' => "Computador Daten funcionando.",
-            'foto' => "/assets/img/daten/funcionando/daten01-func.jpeg",
-        ],
-        [
-            'nome' => "Daten 02",
-            'descricao' => "Computador Daten funcionando.",
-            'foto' => "/assets/img/daten/funcionando/daten02-func.jpeg",
-        ],
-        [
-            'nome' => "Daten 03",
-            'descricao' => "Computador Daten funcionando.",
-            'foto' => "/assets/img/daten/funcionando/daten03-func.jpeg",
-        ],
-        [
-            'nome' => "Daten 04",
-            'descricao' => "Computador Daten funcionando.",
-            'foto' => "/assets/img/daten/funcionando/daten04-func.jpeg",
-        ],
-        [
-            'nome' => "Positivo 01",
-            'descricao' => "Computador Positivo funcionando.",
-            'foto' => "/assets/img/positivo/funcionando/posi01-func.jpeg",
-        ],
-    ];
-    $computadoNaoFunc = [
-        
-        [
-            'nome' => "Daten 01",
-            'descricao' => "Computador Daten não está funcionando.",
-            'foto' => "/assets/img/daten/nao-funcionando/daten01-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Daten 02",
-            'descricao' => "Computador Daten não está funcionando.",
-            'foto' => "/assets/img/daten/nao-funcionando/daten02-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Daten 03",
-            'descricao' => "Computador Daten não está funcionando.",
-            'foto' => "/assets/img/daten/nao-funcionando/daten03-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Daten 04",
-            'descricao' => "Computador Daten não está funcionando.",
-            'foto' => "/assets/img/daten/nao-funcionando/daten04-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Postivo 01",
-            'descricao' => "Computador Positivo não está funcionando.",
-            'foto' => "/assets/img/positivo/nao-funcionando/posi01-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Postivo 02",
-            'descricao' => "Computador Positivo não está funcionando.",
-            'foto' => "/assets/img/positivo/nao-funcionando/posi02-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Postivo 03",
-            'descricao' => "Computador Positivo não está funcionando.",
-            'foto' => "/assets/img/positivo/nao-funcionando/posi03-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Postivo 04",
-            'descricao' => "Computador Positivo não está funcionando.",
-            'foto' => "/assets/img/positivo/nao-funcionando/posi04-nfunc.jpeg",
-        ],
-        [
-            'nome' => "Postivo 05",
-            'descricao' => "Computador Positivo não está funcionando.",
-            'foto' => "/assets/img/positivo/nao-funcionando/posi05-nfunc.jpeg",
-        ],
-    ]
+    $query = $pdo->query(
+        "SELECT situacao, marca, descricao, id, imagem FROM computador"
+    );
+
+    $computador = $query -> fetchALL(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -147,10 +81,12 @@
                     <h2>Computadores Funcionando</h2>
                     <div class="container">
                         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                            <?php foreach ($computadoFunc as $pcFun): ?>
+                            <?php foreach ($computador as $pcFun): ?>
                                 <amostra-prod
-                                    img='<?php echo $pcFun['foto'] ?>'>
-                                    <b><?php echo $pcFun['nome'] ?></b><br>
+                                    img='<?php echo $pcFun['imagem'] ?>'>
+                                    <b><?php echo $pcFun['marca']?>
+                                    <?php echo $pcFun['id']?></b>
+                                    <br>
                                     <?php echo $pcFun['descricao'] ?>
                                 </amostra-prod>
                             <?php endforeach; ?>
@@ -162,13 +98,6 @@
                     <h2>Computadores não Funcionando</h2>
                     <div class="container">
                         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                            <?php foreach ($computadoNaoFunc as $pcNF): ?>
-                                <amostra-prod
-                                    img='<?php echo $pcNF['foto'] ?>'>
-                                    <b><?php echo $pcNF['nome'] ?></b><br>
-                                    <?php echo $pcNF['descricao'] ?>
-                                </amostra-prod>
-                            <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
---
+++ IHS/Linguagem Orientada a Objetos - LOO/aulas/PCBolado/info.md
@@ -0,0 +1,84 @@
+    <!--<?php foreach ($computadoNaoFunc as $pcNF): ?>
+                                <amostra-prod
+                                    img='<?php echo $pcNF['foto'] ?>'>
+                                    <b><?php echo $pcNF['nome'] ?></b><br>
+                                    <?php echo $pcNF['descricao'] ?>
+                                </amostra-prod>
+                            <?php endforeach; ?> -->
+    
+    
+    $computadoFunc = [
+        [
+            'nome' => "Daten 01",
+            'descricao' => "Computador Daten funcionando.",
+            'foto' => "/assets/img/daten/funcionando/daten01-func.jpeg",
+        ],
+        [
+            'nome' => "Daten 02",
+            'descricao' => "Computador Daten funcionando.",
+            'foto' => "/assets/img/daten/funcionando/daten02-func.jpeg",
+        ],
+        [
+            'nome' => "Daten 03",
+            'descricao' => "Computador Daten funcionando.",
+            'foto' => "/assets/img/daten/funcionando/daten03-func.jpeg",
+        ],
+        [
+            'nome' => "Daten 04",
+            'descricao' => "Computador Daten funcionando.",
+            'foto' => "/assets/img/daten/funcionando/daten04-func.jpeg",
+        ],
+        [
+            'nome' => "Positivo 01",
+            'descricao' => "Computador Positivo funcionando.",
+            'foto' => "/assets/img/positivo/funcionando/posi01-func.jpeg",
+        ],
+    ];
+    $computadoNaoFunc = [
+        
+        [
+            'nome' => "Daten 01",
+            'descricao' => "Computador Daten não está funcionando.",
+            'foto' => "/assets/img/daten/nao-funcionando/daten01-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Daten 02",
+            'descricao' => "Computador Daten não está funcionando.",
+            'foto' => "/assets/img/daten/nao-funcionando/daten02-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Daten 03",
+            'descricao' => "Computador Daten não está funcionando.",
+            'foto' => "/assets/img/daten/nao-funcionando/daten03-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Daten 04",
+            'descricao' => "Computador Daten não está funcionando.",
+            'foto' => "/assets/img/daten/nao-funcionando/daten04-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Postivo 01",
+            'descricao' => "Computador Positivo não está funcionando.",
+            'foto' => "/assets/img/positivo/nao-funcionando/posi01-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Postivo 02",
+            'descricao' => "Computador Positivo não está funcionando.",
+            'foto' => "/assets/img/positivo/nao-funcionando/posi02-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Postivo 03",
+            'descricao' => "Computador Positivo não está funcionando.",
+            'foto' => "/assets/img/positivo/nao-funcionando/posi03-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Postivo 04",
+            'descricao' => "Computador Positivo não está funcionando.",
+            'foto' => "/assets/img/positivo/nao-funcionando/posi04-nfunc.jpeg",
+        ],
+        [
+            'nome' => "Postivo 05",
+            'descricao' => "Computador Positivo não está funcionando.",
+            'foto' => "/assets/img/positivo/nao-funcionando/posi05-nfunc.jpeg",
+        ],
+    ]
